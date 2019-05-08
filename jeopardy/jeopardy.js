674a1de47885158a01dd8bb44349c198a14deb37
Elliott <elliott@tom-mac.local>
2019-05-07 20:48:18 -0700

    Added questions for: * Code * Movies * Canada History

---
+++ jeopardy/jeopardy.js
@@ -0,0 +1,42 @@
+
+
+var values = [100, 200,  1000,  1600,  2000];
+var code = [
+    'This is usually abbreviated as  WWW in a URL bar', 
+    'Daily double', 
+    'This code square is named after a general',  // https://upload.wikimedia.org/wikipedia/commons/9/9a/Vigen%C3%A8re_square_shading.svg
+    'A common shift cipher was named after this roman leader, who also has a salad named after him', 
+    'He created the World Wide Web'
+];
+
+var movies = [
+    'In the Lord Of The Rings he serves Frodo',
+    'Mark Watney finds himself stranded on Mars',
+    ' The songs in this rock movie have been recorded, like the album Smell the glove',
+    'Wade Watts searches for Hallidays Egg',
+    'An orphan finds himself among pickpocket friends'
+];
+
+var canadaHistory = [
+    'He built Craigdarroch Castle ',
+    'This railway was built in 1881',
+    'Daily double',
+    'He invented the goalie mask',
+    'Indigenous children were sent to these schools'
+];
+
+var energy = [
+    'energy 1',
+    'energy 2',
+    'energy 3',
+    'energy 4',
+    'energy 5'
+];
+
+var space = [
+    'space 1',
+    'space 2',
+    'space 3',
+    'space 4',
+    'space 5'
+];
